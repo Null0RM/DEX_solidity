00cdeee83830ba9d20577117ad45cfffa1ed9643
Null0RM <jhy_2301@naver.com>
2024-08-28 15:27:39 +0900

    addLiquidity()

--- src/Dex.sol
+++ src/Dex.sol
@@ -2,15 +2,61 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
-contract Dex {
-    constructor(address tokenX, address tokenY) {
+contract Dex is ERC20 {
+    IERC20 public tokenX;
+    IERC20 public tokenY;
 
+    uint256 private reserveX;
+    uint256 private reserveY;
+
+    constructor(address _tokenX, address _tokenY) ERC20("DEX LP TOKEN", "LPT"){
+        tokenX = IERC20(_tokenX);
+        tokenY = IERC20(_tokenY);
     }
 
-    function addLiquidity(uint256 amountX, uint256 amountY, uint256 minLPToken) external returns (uint lpTokens) {}
+    function addLiquidity(uint256 amountX, uint256 amountY, uint256 minLPToken) external returns (uint lpTokens) {
+        require(tokenX.allowance(msg.sender, address(this)) >= amountX, "ERC20: insufficient allowance");
+        require(tokenY.allowance(msg.sender, address(this)) >= amountY, "ERC20: insufficient allowance");
+        require(tokenX.balanceOf(msg.sender) >= amountX, "ERC20: transfer amount exceeds balance");
+        require(tokenY.balanceOf(msg.sender) >= amountY, "ERC20: transfer amount exceeds balance"); 
+
+        tokenX.transferFrom(msg.sender, address(this), amountX);
+        tokenY.transferFrom(msg.sender, address(this), amountY);
+
+        lpTokens = mint(msg.sender, amountX, amountY);
+        require(minLPToken <= lpTokens, "minimum LP too much");
+    }
     
     function removeLiquidity(uint256 lpTokens, uint256 minAmountX, uint256 minAmountY) external returns (uint amountX, uint amountY) {}
     
     function swap(uint256 amountXIn, uint256 amountYIn, uint256 minAmountOut) external returns (uint amountOut)  {}
+
+    function mint(address to, uint256 amountX, uint256 amountY) private returns (uint256 liquidity) {
+        uint256 totalSupply = totalSupply();
+        if (totalSupply == 0) {
+            liquidity = Math.sqrt(amountX * amountY);
+        }
+        else {
+            liquidity = Math.min(amountX * totalSupply / reserveX, amountY * totalSupply / reserveY);
+        }
+
+        require(liquidity > 0, "DEX: INSUFFICIENT_QLIQUIDITY_MINTED");
+        
+        _mint(to, liquidity);
+        update(tokenX.balanceOf(address(this)), tokenY.balanceOf(address(this)));
+        return (liquidity);
+    }
+
+    function getReserve() private view returns (uint256 _reserveX, uint256 _reserveY) {
+        _reserveX = reserveX;
+        _reserveY = reserveY;
+    }
+
+    function update(uint256 _balanceX, uint256 _balanceY) private {
+        reserveX = _balanceX;
+        reserveY = _balanceY;
+    }
 }
